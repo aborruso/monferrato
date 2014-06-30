801f9ab1e2c1cb40299b8635b5a6c2c1dd704384
Andrea Borruso <aborruso@gmail.com>
2014-06-30 23:13:23 +0200

    update

---
+++ data/punti.js
@@ -0,0 +1,53 @@
+var puntigeojson = [
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+			8.206787109375,
+			44.904523389609324
+			]
+		},
+		"properties": {
+			"title": "Asti",
+			"description": "<p>To scan the lines of his face, or feel the bumps on the head of this Leviathan; this is a thing which no Physiognomist or Phrenologist has as yet undertaken.</p>",
+			"marker-color": "#95C92B",
+			"marker-size": "large",
+			"marker-symbol": "monument"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+			8.024139404296875,
+			44.70575417971902
+			]
+		},
+		"properties": {
+			"title": "Alba",
+			"description": "<p>To scan the lines of his face, or feel the bumps on the head of this Leviathan; this is a thing which no Physiognomist or Phrenologist has as yet undertaken.</p>",
+			"marker-color": "#63B6E5",
+			"marker-size": "large",
+			"marker-symbol": "harbor"
+		}
+	},
+	{
+		"type": "Feature",
+		"geometry": {
+			"type": "Point",
+			"coordinates": [
+			8.287811279296875,
+			44.734052347483086
+			]
+		},
+		"properties": {
+			"title": "Canelli",
+			"description": "<p>To scan the lines of his face, or feel the bumps on the head of this Leviathan; this is a thing which no Physiognomist or Phrenologist has as yet undertaken.</p>",
+			"marker-color": "#fc4353",
+			"marker-size": "large",
+			"marker-symbol": "harbor"
+		}
+	}
+	];
